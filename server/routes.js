673610ed11eff6acf92b8d17a374b911314db220
jxckles <dojolesjaier9@gmail.com>
2024-07-27 12:14:06 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("/Login", (req, res) => {
+router.post("https://wildcats-food-express.onrender.com/Login", (req, res) => {
   res.status(200).json({ msg: "POST REQUEST TO https://wildcats-food-express.onrender.com/login" });
 });
 
-router.post("/register", (req, res) => {
+router.post("https://wildcats-food-express.onrender.com/register", (req, res) => {
   res.status(200).json({ msg: "POST REQUEST TO https://wildcats-food-express.onrender.com/register" });
 });
 
-router.get("/admin", (req, res) => {
+router.get("https://wildcats-food-express.onrender.com/admin", (req, res) => {
   res.status(200).json({ msg: "GET REQUEST TO https://wildcats-food-express.onrender.com/admin" });
 });
 
-router.get("/dashboard", (req, res) => {
+router.get("https://wildcats-food-express.onrender.com/dashboard", (req, res) => {
   res.status(200).json({ msg: "GET REQUEST TO https://wildcats-food-express.onrender.com/dashboard" });
 });
 
-router.get("/client-interface", (req, res) => {
+router.get("https://wildcats-food-express.onrender.com/client-interface", (req, res) => {
   res.status(200).json({ msg: "GET REQUEST TO https://wildcats-food-express.onrender.com/client-interface" });
 });
 
-router.post("/logout", (req, res) => {
+router.post("https://wildcats-food-express.onrender.com/logout", (req, res) => {
   res.status(200).json({ msg: "POST REQUEST TO https://wildcats-food-express.onrender.com/logout" });
 });
 
-router.post("/forgot-password", (req, res) => {
+router.post("https://wildcats-food-express.onrender.com/forgot-password", (req, res) => {
   res.status(200).json({ msg: "POST REQUEST TO https://wildcats-food-express.onrender.com/forgot-password" });
 });
 
