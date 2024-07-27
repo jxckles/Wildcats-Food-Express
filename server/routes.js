966158524603a4c49da6a8ff8947f8ca06f0f70a
jxckles <dojolesjaier9@gmail.com>
2024-07-27 11:28:01 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("/login", (req, res) => {
+router.post("/Login", (req, res) => {
   res.status(200).json({ msg: "POST REQUEST TO /api/login" });
 });
 
